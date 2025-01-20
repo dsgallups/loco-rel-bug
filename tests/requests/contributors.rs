3e37b9b2cc60b5cdb1a45a67152b58a3380ecd6f
dsgallups <dsgallups@protonmail.com>
2025-01-20 10:30:50 -0500

    scaffold contributors

---
+++ tests/requests/contributors.rs
@@ -0,0 +1,16 @@
+use loco_rel_bug::app::App;
+use loco_rs::testing::prelude::*;
+use serial_test::serial;
+
+#[tokio::test]
+#[serial]
+async fn can_get_contributors() {
+    request::<App, _, _>(|request, _ctx| async move {
+        let res = request.get("/api/contributors/").await;
+        assert_eq!(res.status_code(), 200);
+
+        // you can assert content like this:
+        // assert_eq!(res.text(), "content");
+    })
+    .await;
+}
